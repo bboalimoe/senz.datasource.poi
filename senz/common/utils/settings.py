ed314a005f8b901d4bdab578dec3e51d0320eda8
wuzhifan <wuzhifan@cecgw.com>
2015-03-11 18:06:37 +0800

    merge spider code and add function of store LocationRecognition data

---
+++ senz/common/utils/settings.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# use (id,key) or X-AVOSCloud-Request-Sign to auth
+# https://cn.avoscloud.com/docs/rest_api.html
+
+#avos_app_id = 'vigxpgtjk8w6ruxcfaw4kju3ssyttgcqz38y6y6uablqivjd'
+#avos_app_key = 'dxbawm2hh0338hb37wap59gticgr92dpajd80tzekrgv1ptw'
+#avos_app_master_key = 'u0nu3suqria905en9gbq7isetlf5exoqmndv4fxcfck26kdr'
+
+#todo: dispatch modules configurations in system init
+
+groups = {
+    'origin' : {
+        'avos_id' : 'vigxpgtjk8w6ruxcfaw4kju3ssyttgcqz38y6y6uablqivjd',
+        'avos_key': 'dxbawm2hh0338hb37wap59gticgr92dpajd80tzekrgv1ptw',
+        'avos_master_key' : 'u0nu3suqria905en9gbq7isetlf5exoqmndv4fxcfck26kdr',
+        'avos_classes_list' : []
+    },
+    'location' : {
+        'avos_id' : 'h1ii4cs01bqlc94at5l3rzngwmiembappirqdo22z2p1e610',
+        'avos_key': 'qmdhudhx52mnr68euhiwavsloekaox3xp120lflvo5f91do5',
+        'avos_master_key' : 'n2tskry5otv71ldaj0sylevhfe2h2uww0l3d0bk71mqffiz3',
+        'avos_classes_list' : ['LocationRecognition',]
+    }
+}
---
+++ senz/common/utils/translate.py
@@ -0,0 +1,74 @@
+# -*- encoding=utf-8 -*-
+__author__ = 'zhanghengyang'
+
+class Trans(object):
+
+    def __init__(self):
+
+        self.ac = activity_dict
+        self.poi = poitype_dict
+
+
+    def activity_trans(cls, name):
+
+        if not isinstance(name, str) and not isinstance(name, unicode):
+
+            raise Exception("name should be str or unicode")
+
+        else:
+
+            return cls.ac[name]
+
+
+    def poitype_trans(cls, name):
+
+        if not isinstance(name, str) and not isinstance(name, unicode):
+
+            raise Exception("name should be str or unicode")
+
+        else:
+
+            return cls.poi[name]
+
+
+
+
+
+activity_dict = {
+    u"音乐" : "music",
+    u"戏剧" : "opera",
+    u"讲座" : "lecture",
+    u"聚会" : "party",
+    u"电影" : "film",
+    u"展览" : "exhibition",
+    u"运动" : "sport",
+    u"公益" : "public",
+    u"旅行" : "travel",
+    u"其他" : "other",
+
+}
+
+
+poitype_dict = {
+
+    u"休闲娱乐":"leisure",
+    u"地产小区":"neighborhood",
+    u"政府机构":"government",
+    u"餐饮":"canteen",
+    u"教育":"education",
+    u"交通设施":"transportation",
+    u"金融":"finance",
+    u"行政地标":"landmark",
+    u"旅游景点":"scene",
+    u"其他":"other",
+    u"房地产":"estate",
+    u"other":"other",
+    u"美食":"tasty",
+    u"生活服务":"service",
+    u"公司企业":"company",
+    u"购物":"shopping",
+    u"教育培训":"training",
+    u"汽车服务":"car",
+    u"医疗":"hospital",
+
+}
