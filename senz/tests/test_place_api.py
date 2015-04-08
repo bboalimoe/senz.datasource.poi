310fb1c29587fd96c6cb44a81c6a720006155a56
wuzhifan <wuzhifan@cecgw.com>
2015-04-08 16:32:41 +0800

    add places api

--- senz/tests/test_place_api.py
+++ senz/tests/test_place_api.py
@@ -28,8 +28,3 @@ if __name__ == '__main__':
     testor = TestPlaceApi()
     testor.testLocTag()
     #testor.testAddNearTag()
-
-{"results": [{"status": "", "ratio": 0.9242718446601942, "objectId": "5524df9de4b087afeb152802", "estimateTime": 11020000, "userId": "2b4e710aab89f6c5", "longitude": 116.34389969678843, "latitude": 39.8972862516112, "tag": "home", "updatedAt": "2015-04-08T07:58:21.859Z", "date": "", "createdAt": "2015-04-08T07:58:21.859Z"},
-             {"status": "", "ratio": 0.9544303797468354, "objectId": "5524df9de4b087afeb152803", "estimateTime": 11020000, "userId": "2b4e710aab89f6c5", "longitude": 116.34389969678843, "latitude": 39.8972862516112, "tag": "office", "updatedAt": "2015-04-08T07:58:21.902Z", "date": "", "createdAt": "2015-04-08T07:58:21.902Z"}]}
-
-{"results": "[{\"latitude\": 39.8972862516112, \"estimateTime\": 11020000, \"longitude\": 116.34389969678843, \"tags\": [{\"estimateTime\": 4760000, \"tag\": \"home\", \"ratio\": 0.9242718446601942}, {\"estimateTime\": 3770000, \"tag\": \"office\", \"ratio\": 0.9544303797468354}]}]"}
